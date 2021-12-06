41dac5d6df1eb5b535ef4c7e2a39d6abdbb473b2
FalseR20 <kno3.2002@gmail.com>
2021-12-06 15:20:25 +0300

    DM3: add prim

--- DM/3/ost_trees.py
+++ DM/3/ost_trees.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 
+def prim(matrix: np.ndarray) -> list:
+    roots = [0]
+    tuples = []
+    while len(roots) < len(matrix):
+        min_el = np.inf
+        min_i = np.inf
+        min_j = np.inf
+        for root in roots:
+            for j, w in enumerate(matrix[root]):
+                if 0 < w < min_el and j not in roots:
+                    min_el = w
+                    min_j = j
+                    min_i = root
+        roots.append(min_j)
+        tuples += [(min_i, min_j,)]
+    return tuples
+
+
 def main():
     matrix = np.array([
         [0, 5, 2, 3, 0, 0],
@@ -9,7 +27,9 @@ def main():
         [3, 3, 2, 0, 7, 5],
         [0, 0, 6, 7, 0, 4],
         [0, 0, 0, 5, 4, 0],
-    ], dtype=int)
+    ])
+    print("Default matrix:", matrix, sep='\n')
+    print("Ost tree:", prim(matrix), sep='\n')
 
 
 if __name__ == '__main__':
