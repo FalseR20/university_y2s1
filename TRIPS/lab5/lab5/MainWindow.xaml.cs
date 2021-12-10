42010d0bfedccf0e69a0928b7cd62db45871a1f3
FalseR20 <kno3.2002@gmail.com>
2021-12-10 20:32:17 +0300

    add trips lab5

---
+++ TRIPS/lab5/lab5/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace lab5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Focus(object sender, EventArgs e)
+        {
+            ((FrameworkElement)sender).Style = (Style)Resources["Focus"];
+        }
+
+        private void FocusOff(object sender, EventArgs e)
+        {
+            ((FrameworkElement)sender).Style = (Style)Resources["UnFocus"];
+        }
+    }
+}
